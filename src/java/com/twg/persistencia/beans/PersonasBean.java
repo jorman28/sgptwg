4678d377816140fbd9c7b9e28bf520e1b38459bf
Erika Jhoana <Erika Jhoana@Lenovo-PC>
2015-07-06 20:10:33 -0500

    Se agrega main para probar la conexión a la BD.

---
+++ src/java/com/twg/persistencia/beans/PersonasBean.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.twg.persistencia.beans;
+
+/**
+ *
+ * @author Erika Jhoana
+ */
+public class PersonasBean {
+
+    private int id;
+    private String documento;
+    private String tipo_documento;
+    private String nombres;
+    private String apellidos;
+    private String direcion;
+    private String telefono;
+    private String celular;
+    private String correo;
+    
+    public PersonasBean(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getTipo_documento() {
+        return tipo_documento;
+    }
+
+    public void setTipo_documento(String tipo_documento) {
+        this.tipo_documento = tipo_documento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDirecion() {
+        return direcion;
+    }
+
+    public void setDirecion(String direcion) {
+        this.direcion = direcion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+    
+}
---
+++ src/java/com/twg/persistencia/beans/TiposDocumentosBean.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.twg.persistencia.beans;
+
+/**
+ *
+ * @author Erika Jhoana
+ */
+public class TiposDocumentosBean {
+    
+    private String tipo;
+    private String nombre;
+    
+    public TiposDocumentosBean(){
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
