23baf2c23a2a7a8f2fdc2be93f9f700d0eb54ebf
jorman28 <jorman_rincon24122@elpoli.edu.co>
2015-12-01 00:53:48 -0500

    Actividades
    Creación de Actividades Controller

--- src/java/com/twg/controladores/Actividades_EsfuerzosController.java
+++ src/java/com/twg/controladores/Actividades_EsfuerzosController.java
@@ -2,6 +2,11 @@ package com.twg.controladores;
 
 import javax.servlet.http.HttpServlet;
 
+/**
+ *
+ * @author Jorman Rincón
+ */
+
 public class Actividades_EsfuerzosController extends HttpServlet {
 
     
