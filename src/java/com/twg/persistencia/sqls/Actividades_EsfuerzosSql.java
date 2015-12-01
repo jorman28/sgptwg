51aa5d527edd5642825bf58dd8ae8499d03a0621
jorman28 <jorman_rincon24122@elpoli.edu.co>
2015-11-30 22:53:38 -0500

    Actividades Esfuerzos
    Se añaden clases en cada capa de Actividades Esfuerzos

---
+++ src/java/com/twg/persistencia/sqls/Actividades_EsfuerzosSql.java
@@ -0,0 +1,32 @@
+package com.twg.persistencia.sqls;
+
+/**
+ *
+ * @author Jorman
+ */
+public class Actividades_EsfuerzosSql {
+
+    public String contarActividades_Esfuerzos() {
+        return "SELECT COUNT(*) FROM actividades_esfuerzos";
+    }
+
+    public String consultarActividades_Esfuerzos(Integer id) {
+        String sql = "SELECT * FROM actividades_esfuerzos WHERE fecha_eliminacion IS NULL ";
+        if (id != null) {
+            sql += "AND id = " + id + " ";
+        }
+        return sql;
+    }
+
+    public String insertarActividad_Esfuerzo() {
+        return "INSERT INTO actividades_esfuerzos (actividad, empleado, fecha, tiempo, descripcion) VALUES (?, ?, ?, ?, ?)";
+    }
+
+    public String actualizarActividad_Esfuerzo() {
+        return "UPDATE actividades_esfuerzos SET actividad = ?, empleado = ?, fecha = ?, tiempo = ?, descripcion = ? WHERE id = ?";
+    }
+
+    public String eliminarActividad_Esfuerzo() {
+        return "UPDATE actividades_esfuerzos SET fecha_eliminacion = now() WHERE id = ?";
+    }
+}
