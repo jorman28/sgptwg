0235b2f083dd641bcc244dcf651d5a5a57d5842a
erikasta07 <erikasta07@hotmail.com>
2015-12-08 17:13:05 -0500

    SQLs para cambiar el tipo de dato de los campos de tiempo, con el fin de que sean decimales. Se elimina y se vueve a crear correctamente la foránea de la columna estado, con la tabla de estados. La que había anteriormente la estaba asociando contra la versión.

--- src/java/com/twg/utilidades/sgptwg_08.sql
+++ src/java/com/twg/utilidades/sgptwg_08.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `actividades` CHANGE `tiempo_estimado` `tiempo_estimado` DECIMAL(11,1) NOT NULL, CHANGE `tiempo_invertido` `tiempo_invertido` DECIMAL(11,1) NULL DEFAULT NULL;
 ALTER TABLE actividades DROP FOREIGN KEY actividades_estados_fk;
-ALTER TABLE actividades ADD FOREIGN KEY (estado) REFERENCES estados(id); 
+ALTER TABLE actividades ADD FOREIGN KEY (estado) REFERENCES estados(id);  
