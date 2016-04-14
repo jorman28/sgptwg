a3f1abda5fa7140bf2b5a87484f27449c3e92c99
andres-giraldo <andres_giraldo24131@elpoli.edu.co>
2016-04-13 21:08:13 -0500

    Ajustes en SQL de verisión

--- src/java/com/twg/utilidades/sqls/sgptwg_06.sql
+++ src/java/com/twg/utilidades/sqls/sgptwg_06.sql
@@ -1,2 +1,39 @@
-ALTER TABLE proyectos ADD CONSTRAINT proyectos_uk UNIQUE KEY (nombre);
-ALTER TABLE versiones ADD CONSTRAINT versiones_uk UNIQUE KEY (proyecto, nombre);
+DELETE pxp FROM
+    paginas pag
+        INNER JOIN
+    permisos per ON per.pagina = pag.id
+        INNER JOIN
+    permisos_perfiles pxp ON pxp.permiso = per.id WHERE pag.nombre = 'Cerrar sesión';
+    
+DELETE per FROM paginas pag
+        INNER JOIN
+    permisos per ON per.pagina = pag.id 
+WHERE
+    pag.nombre = 'Cerrar sesión';
+    
+DELETE FROM paginas 
+WHERE
+    nombre = 'Cerrar sesión';
+    
+DELETE pxp FROM
+    paginas pag
+        INNER JOIN
+    permisos per ON per.pagina = pag.id
+        INNER JOIN
+    permisos_perfiles pxp ON pxp.permiso = per.id WHERE pag.nombre = 'Continuar';
+    
+DELETE per FROM paginas pag
+        INNER JOIN
+    permisos per ON per.pagina = pag.id 
+WHERE
+    pag.nombre = 'Continuar';
+    
+DELETE FROM paginas 
+WHERE
+    nombre = 'Continuar';
+
+UPDATE paginas SET url='/PermisosController' WHERE id='4';
+UPDATE paginas SET url='/CargosController' WHERE id='7';
+UPDATE paginas SET url='/EstadosController' WHERE id='8';
+UPDATE paginas SET url='/ProyectosController' WHERE id='10';
+UPDATE paginas SET url='/ActividadesController' WHERE id='11';
--- src/java/com/twg/utilidades/sqls/sgptwg_13.sql
+++ src/java/com/twg/utilidades/sqls/sgptwg_13.sql
@@ -1,2 +1,3 @@
 INSERT INTO paginas (id, nombre, url, grupo) VALUES (14, 'Actividades por estado', '/ActividadesPorEstadoController', 13);
 INSERT INTO permisos (id,pagina,permiso) VALUES (45,14,'ACCESO');
+INSERT INTO permisos_perfiles (permiso, perfil) VALUES (45, 1);
